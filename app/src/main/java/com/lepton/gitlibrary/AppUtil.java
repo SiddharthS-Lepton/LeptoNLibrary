99260a65373173c769e3a94d12a79b8b632f45ce
Siddharth <siddharth.sharma@leptonmaps.com>
2021-12-16 16:57:29 +0530

    'app'

---
+++ app/src/main/java/com/lepton/gitlibrary/AppUtil.java
@@ -0,0 +1,9 @@
+package com.lepton.gitlibrary;
+
+
+public class AppUtil {
+
+    public int addSum(int val1,int val2){
+        return val1+val2;
+    }
+}
---
+++ leptonlibrary/src/main/java/com/lepton/leptonlibrary/AppUtil.java
@@ -0,0 +1,13 @@
+package com.lepton.leptonlibrary;
+
+
+public class AppUtil {
+
+    public int addSum(int val1,int val2){
+        return val1+val2;
+    }
+
+    public int subtract(int val1,int val2){
+        return val1-val2;
+    }
+}
